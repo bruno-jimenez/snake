dc187e5ff1865a20d4ed546ad65848c5e79422e2
unknown <bruno-jimenez@laplateforme.io>
2023-02-14 17:18:32 +0100

    adding file with global fonction

---
+++ snake.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from pygame import *
+
+#[---------------------------------]
+#      creating the window
+#[---------------------------------]
+
+root = Tk()
+root.title("Snake")
+root.resizable(width=False, height=False)
+root.geometry("500x400")
+
+#[---------------------------------]
+#      spawn & respawn apple
+#[---------------------------------]
+
+def apple_generator():
+
+    pass
+
+#[---------------------------------]
+#    fonction for feeding snake
+#[---------------------------------]
+
+def feeding():
+
+    pass
+
+#[---------------------------------]
+#  fonction for bind arrow key
+#[---------------------------------]
+
+def binding():
+
+    pass
+
+#[---------------------------------]
+#   movement and refresh sprite
+#[---------------------------------]
+
+def movement():
+
+    pass
+
+
+root.mainloop()
