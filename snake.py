da59d10d1ddd6b877e9bf88cd3210c757631df45
unknown <bruno-jimenez@laplateforme.io>
2023-02-16 13:16:37 +0100

    adding functionnal snake without IA and great UI

--- snake.py
+++ snake.py
@@ -3,6 +3,7 @@ import pygame
 import random
 from pygame import event
 import time
+from sys import exit
 
 #[---------------------------------]
 # creating the window and setup 
@@ -10,14 +11,12 @@ import time
 
 pygame.init()
 pygame.font.init()
-speed = 15
+speed = 10
 fps = pygame.time.Clock()
 
 # window setup
-root = Tk()
-root.title("Snake")
-x = 500
-y = 400
+x=720
+y=480
 window = pygame.display.set_mode((x, y))
 pygame.display.set_caption("Snake")
 font = pygame.font.Font(None, 35)
@@ -40,101 +39,11 @@ score = 0
 score_increment = 1
 last_score = ''
 
-# Return a random number for spawn a fruit into the screen
-x=400
-y=500
-fruit_spawn = False
-fruit = [random.randrange(1, (x//10))*10, random.randrange(1, (y//10))*10]
-
-# Create the physical form of the snake and fruits
-for pos in body:
-    pygame.draw.rect(window, green, pygame.Rect(pos[0], pos[1], 10, 10))
-    pygame.draw.rect(window, red, pygame.Rect(fruit[0], fruit[1], 10, 10))
-
-#[---------------------------------]
-#    creating the loop
-#[---------------------------------]
-
-def global_loop():
-    global Running
-    global direction
-    global change_to
-    global body
-    global position
-    global last_score
-    global score
-
-while Running:
-        for event in pygame.event.get():
-
-            # Quit the program if close button is clicked
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-
-#[---------------------------------]
-#    fonction for feeding snake
-#[---------------------------------]
-
-        def feeding():
-            if position[0] == fruit[0] and position[1] == fruit[1]:
-                fruit_spawn = False
-                score += score_increment
-            else:
-                body.pop()        
-        
-
-
-
-#[---------------------------------]
-#      spawn & respawn apple
-#[---------------------------------]
-
-        def apple_generator():
-            if not fruit_spawn:
-                fruit = [random.randrange(1, (x // 10)) * 10, random.randrange(1, (y // 10)) * 10]
-            fruit_spawn = True
-        
-
-#[---------------------------------]
-#  fonction for bind arrow key
-#[---------------------------------]
-
-        def binding():
-    
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    change_to = 'LEFT'
-                if event.key == pygame.K_UP:
-                    change_to = 'UP'
-                if event.key == pygame.K_RIGHT:
-                    change_to = 'RIGHT'
-                if event.key == pygame.K_DOWN:
-                    change_to = 'DOWN'
-
-            if change_to == 'UP' and direction != 'DOWN':
-                direction = 'UP'
-            if change_to == 'DOWN' and direction != 'UP':
-                direction = 'DOWN'
-            if change_to == 'LEFT' and direction != 'RIGHT':
-                direction = 'LEFT'
-            if change_to == 'RIGHT' and direction != 'LEFT':
-                direction = 'RIGHT'    
-        
-
-#[---------------------------------]
-#   movement and refresh sprite
-#[---------------------------------]
-
-        def movement():
-            if direction == 'UP':
-                position[1] -= 10
-            if direction == 'DOWN':
-                position[1] += 10
-            if direction == 'LEFT':
-                position[0] -= 10
-            if direction == 'RIGHT':
-                position[0] += 10
+# Return a random number for spawn a apple into the screen
+x=720
+y=480
+apple_spawn = False
+apple = [random.randrange(1, (x//10))*10, random.randrange(1, (y//10))*10]
 
 
 def game_over():        # End the game when condition is trigger
@@ -143,7 +52,7 @@ def game_over():        # End the game when condition is trigger
     # Game over text layout
     last_score = score
     game_over_text = pygame.font.Font(None, 50)
-    game_over_frame = game_over_text.render('GAME OVER' + '  ' + f'Your score: {score}', True, white)
+    game_over_frame = game_over_text.render('GAME OVER' + '  ' + f'Your score : {score}', True, white)
     game_over_set = game_over_frame.get_rect()
     game_over_set.midtop = (x / 2, y / 4)
 
@@ -158,7 +67,132 @@ def game_over():        # End the game when condition is trigger
     game_menu()
 
 
-def game_menu():        # Menu displaying last score and reset the game
+# Create the physical form of the snake and apples
+for pos in body:
+    pygame.draw.rect(window, green, pygame.Rect(pos[0], pos[1], 10, 10))
+    pygame.draw.rect(window, red, pygame.Rect(apple[0], apple[1], 10, 10))
+
+#[---------------------------------]
+#    creating the loop
+#[---------------------------------]
+
+def main_loop():    # Main game loop
+    global change_to
+    global direction
+    global apple
+    global apple_spawn
+    global score
+
+    # Main loop
+    while Running:
+        for event in pygame.event.get():
+
+            # Quit the program if close button is clicked
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+#[---------------------------------]
+#         bind arrow key
+#[---------------------------------]
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    change_to = 'LEFT'
+                if event.key == pygame.K_UP:
+                    change_to = 'UP'
+                if event.key == pygame.K_RIGHT:
+                    change_to = 'RIGHT'
+                if event.key == pygame.K_DOWN:
+                    change_to = 'DOWN'
+
+        if change_to == 'UP' and direction != 'DOWN':
+            direction = 'UP'
+        if change_to == 'DOWN' and direction != 'UP':
+            direction = 'DOWN'
+        if change_to == 'LEFT' and direction != 'RIGHT':
+            direction = 'LEFT'
+        if change_to == 'RIGHT' and direction != 'LEFT':
+            direction = 'RIGHT'
+
+#[---------------------------------]
+#   movement and refresh sprite
+#[---------------------------------]
+
+        if direction == 'UP':
+            position[1] -= 10
+        if direction == 'DOWN':
+            position[1] += 10
+        if direction == 'LEFT':
+            position[0] -= 10
+        if direction == 'RIGHT':
+            position[0] += 10
+
+        body.insert(0, list(position))
+
+
+#[---------------------------------]
+#    feeding snake and grow it
+#[---------------------------------]
+
+        if position[0] == apple[0] and position[1] == apple[1]:
+            apple_spawn = False
+            score += score_increment
+        else:
+            body.pop()
+
+#[---------------------------------]
+#      spawn & respawn apple
+#[---------------------------------]
+
+        if not apple_spawn:
+            apple = [random.randrange(1, (x // 10)) * 10, random.randrange(1, (y // 10)) * 10]
+            apple_spawn = True
+
+#[---------------------------------]
+#   remove back of snake when move
+#[---------------------------------]
+
+        window.fill(black)
+
+#[---------------------------------]
+#  create shape for snake and apple
+#[---------------------------------]
+
+        for pos in body:
+            pygame.draw.rect(window, green, pygame.Rect(pos[0], pos[1], 10, 10))
+            pygame.draw.rect(window, red, pygame.Rect(apple[0], apple[1], 10, 10))
+
+
+#[------------------------------------------]
+#   condition of losing (wall & self touch )
+#[------------------------------------------]
+
+        if position[0] < 0 or position[0] > x - 10:
+            game_over()
+        if position[1] < 0 or position[1] > y - 10:
+            game_over()
+
+
+        for self in body[1:]:
+            if position[0] == self[0] and position[1] == self[1]:
+                game_over()
+
+#[---------------------------------]
+#      write score
+#[---------------------------------]
+
+        score_text = font.render(f'Score: {score}', True, (255, 255, 255))
+        window.blit(score_text, (50, 50))
+
+#[---------------------------------]
+#      set the game refresh
+#[---------------------------------]
+        pygame.display.flip()
+        fps.tick(speed)
+
+
+def game_menu():       
     global Running
     global direction
     global change_to
@@ -167,14 +201,14 @@ def game_menu():        # Menu displaying last score and reset the game
     global last_score
     global score
 
-    # Reset game
+  
     Running = False
     body = [[100, 50], [90, 50], [80, 50], [70, 50]]
     position = [100, 50]
     direction = 'RIGHT'
     change_to = direction
 
-    # Press space to play
+
     while not Running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -183,9 +217,9 @@ def game_menu():        # Menu displaying last score and reset the game
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     Running = True
-                    mainloop()
+                    main_loop()
 
-        # Game_menu text layout
+        
         game_menu_text = pygame.font.Font(None, 50)
         game_menu_frame = game_menu_text.render('PRESS SPACE TO START THE GAME', True, white)
         game_menu_set = game_menu_frame.get_rect()
@@ -195,11 +229,10 @@ def game_menu():        # Menu displaying last score and reset the game
         score_set.midbottom = (x / 2, y / 4)
 
 
-        # Display the text
         window.blit(game_menu_frame, game_menu_set)
         window.blit(score_frame, score_set)
 
-        # Refresh the screen
+
         pygame.display.flip()
         window.fill(black)
 
